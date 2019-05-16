b16fb0cf3c78ba4e9616829ab2458528588a1677
4tochka <admin@bitaps.com>
2019-05-16 12:22:09 +0300

    connector

--- pybtc/__init__.py
+++ pybtc/__init__.py
@@ -7,7 +7,7 @@ from .block import *
 from .address import *
 from .wallet import *
 from .crypto import *
-from cache_strategies import LRU
+# from cache_strategies import LRU
 from cache_strategies import MRU
 from pybtc.connector import Connector
 
